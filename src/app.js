628435ea26f83e2a79a663a7589e7a87b83b4ab4
letoor1234 <angelmartinezwb@gmail.com>
2023-02-19 20:31:24 -0300

    chore: add example

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const express = require("express");
 const app = express();
 
 // Agregamos rutas
+/* 
+Ejemplo:
+  app.get("/cart", (req, res) => res.send("Estoy en el carrito"))
+*/
 const authRoutes = require("./routes/auth.route");
 const homeRoutes = require("./routes/home.route");
 const productDetailRoutes = require("./routes/productDetail.route");
